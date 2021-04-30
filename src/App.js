350372ee3b7a9c439a7d6253150d715516a37bbd
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-04-30 10:41:48 -0300

    Començando a componentizaçao

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar"
+import Corpo from "./Corpo"
+import Fundo from "./Fundo"
+
+export default function App(){
+    return(
+        <>
+            <Navbar />
+
+            <Corpo />
+
+            <Fundo />
+      </>
+    );
+}
